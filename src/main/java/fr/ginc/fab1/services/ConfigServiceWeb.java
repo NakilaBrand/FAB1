d1080b1fdf7db58da4ecffe0956ee1b832bbf78b
Mika <mickael.quintin2018@campus-eni.fr>
2018-12-04 10:22:07 +0100

    Services added

---
+++ src/main/java/fr/ginc/fab1/services/ConfigServiceWeb.java
@@ -0,0 +1,10 @@
+package fr.ginc.fab1.services;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/service/rest")
+
+public class ConfigServiceWeb extends Application {
+
+}
