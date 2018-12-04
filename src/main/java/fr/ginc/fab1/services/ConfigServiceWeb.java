0dec6862bfec6c93683600c0c90c390d2bef40a0
Mika <mickael.quintin2018@campus-eni.fr>
2018-12-04 11:50:09 +0100

    Webservice updated

--- src/main/java/fr/ginc/fab1/services/ConfigServiceWeb.java
+++ src/main/java/fr/ginc/fab1/services/ConfigServiceWeb.java
@@ -3,7 +3,7 @@ package fr.ginc.fab1.services;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/service/rest")
+@ApplicationPath("/service")
 
 public class ConfigServiceWeb extends Application {
 
