aa12f46c17919425d6dee01bdcfe4c38971a307d
Misx <nicolas.amaralferreira2018@campus-eni.fr>
2018-12-05 15:05:51 +0100

    changement design btn carte

--- src/main/webapp/js/carte.js
+++ src/main/webapp/js/carte.js
@@ -14,7 +14,7 @@ function afficherListe() {
         data.plats.forEach(function (plat) {
             var div = document.createElement('div');
             div.setAttribute('class', "col-12 col-sm-6 col-md-4")
-            div.innerHTML = "<div class=\"plat\"> <h3 class=\"plats__title\">" + plat.nom + "</h3> <a href=\"avis.html\"><button class=\"buttonNote\" type=\"rsvBtn\">Noter</button></a><img src=\"icons/AjouterNoir.png\" height=\"30\"/><div class=\"plats__transform\"><img class=\"imgCarte\" src=\"" + plat.imageURL + "\" alt=\"" + plat.nom + "\"></div></div>"
+            div.innerHTML = "<div class=\"plat\"> <h3 class=\"plats__title\">" + plat.nom + "</h3> <a href=\"avis.html\"><button class=\"buttonNote\" type=\"rsvBtn\">Noter</button></a><button class=\"buttonNote\" type=\"rsvBtn\" onclick=\"ajouterPanier("+plat.id+")\"><img src=\"icons/AjouterNoir.png\" height=\"10\"/></button><div class=\"plats__transform\"><img class=\"imgCarte\" src=\"" + plat.imageURL + "\" alt=\"" + plat.nom + "\" ></div></div>"
             listePlat.appendChild(div);
         })
 
