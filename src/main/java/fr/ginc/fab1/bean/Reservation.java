a439ca296e4b39b2bf54119f63f64f4b001e1a6c
Mika <mickael.quintin2018@campus-eni.fr>
2018-12-06 14:10:26 +0100

    Reservation implemented

--- src/main/java/fr/ginc/fab1/bean/Reservation.java
+++ src/main/java/fr/ginc/fab1/bean/Reservation.java
@@ -3,6 +3,8 @@ package fr.ginc.fab1.bean;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @javax.persistence.Entity
 
 public class Reservation implements java.io.Serializable
@@ -13,25 +15,26 @@ public class Reservation implements java.io.Serializable
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private java.lang.Integer id;
     
-    private java.util.Date Jour;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private java.util.Date jour;
     
     private java.lang.Integer nbPersonne;
     
 
     
     @javax.persistence.ManyToOne(
-        cascade = {javax.persistence.CascadeType.ALL},
     	fetch = javax.persistence.FetchType.LAZY,
     	optional = false
     )
     Restaurant restaurant;
+    
     @javax.persistence.ManyToOne(
-        cascade = {javax.persistence.CascadeType.ALL},
     	fetch = javax.persistence.FetchType.LAZY
     )
     TrancheHoraire horaireSelectionne;
+    
     @javax.persistence.ManyToOne(
-        cascade = {javax.persistence.CascadeType.ALL},
+     
     	fetch = javax.persistence.FetchType.LAZY
     )
     Utilisateur utilisateur;
@@ -41,18 +44,16 @@ public class Reservation implements java.io.Serializable
         this.id = id;
     }
     
-    
     public java.lang.Integer getId() {
         return this.id;
     }
     
     public void setJour(java.util.Date Jour) {
-        this.Jour = Jour;
+        this.jour = Jour;
     }
     
-    
     public java.util.Date getJour() {
-        return this.Jour;
+        return this.jour;
     }
     
     public void setNbPersonne(java.lang.Integer nbPersonne) {
@@ -91,6 +92,14 @@ public class Reservation implements java.io.Serializable
     public Utilisateur getUtilisateur() {
         return this.utilisateur;
     }
+
+	@Override
+	public String toString() {
+		return "Reservation [id=" + id + ", Jour=" + jour + ", nbPersonne=" + nbPersonne + ", restaurant=" + restaurant
+				+ ", horaireSelectionne=" + horaireSelectionne + ", utilisateur=" + utilisateur + "]";
+	}
+    
+    
     
 }
 
