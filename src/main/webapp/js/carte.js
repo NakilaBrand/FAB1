f0610f4749b6570b573bb69abe57f45049b60a4a
Misx <nicolas.amaralferreira2018@campus-eni.fr>
2018-12-06 09:28:57 +0100

    style carte

--- src/main/webapp/js/carte.js
+++ src/main/webapp/js/carte.js
@@ -14,7 +14,12 @@ function afficherListe() {
         data.plats.forEach(function (plat) {
             var div = document.createElement('div');
             div.setAttribute('class', "col-12 col-sm-6 col-md-4")
-            div.innerHTML = "<div class=\"plat\"> <h3 class=\"plats__title\">" + plat.nom + "</h3> <a href=\"avis.html\"><button class=\"buttonNote\" type=\"rsvBtn\">Noter</button></a><button class=\"buttonNote\" type=\"rsvBtn\" onclick=\"ajouterPanier("+plat.id+")\"><img src=\"icons/AjouterNoir.png\" height=\"15\"/></button><div class=\"plats__transform\"><img class=\"imgCarte\" src=\"" + plat.imageURL + "\" alt=\"" + plat.nom + "\" ></div></div>"
+            div.innerHTML = "<div class=\"plat\"> <h3 class=\"plats__title\">" + plat.nom + "</h3> <a href=\"avis.html\"><button class=\"buttonNote\" type=\"rsvBtn\">Noter</button></a><button class=\"buttonNote\" type=\"rsvBtn\" onclick=\"ajouterPanier("+plat.id+")\"><img src=\"icons/AjouterNoir.png\" height=\"15\"/></button><div class=\"plats__transform\"></div></div>"
+            
+            //<img class=\"imgCarte\" src=\"" + plat.imageURL + "\" alt=\"" + plat.nom + "\" >
+            div.style.backgroundImage = "url(\""+plat.imageURL+"\")";
+            div.style.height = "400px";
+            div.style.backgroundSize = "cover";
             listePlat.appendChild(div);
         })
 
